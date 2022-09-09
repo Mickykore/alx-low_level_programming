6f4071c83a23c57b10963ee1ac72114788ed619d
Mickykore <mikiyasayele6@gmail.com>
2022-09-09 22:39:23 +0000

    add file 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+int n;
+for (n = 48; n <= 57; ++n)
+{
+putchar(n);
+}
+for (c = 'a'; n <= 'f'; ++n)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
