5ab377fc3393d7b092b9911fa53b30fff08c92f7
Mickykore <mikiyasayele6@gmail.com>
2022-09-09 20:07:10 +0000

    edit file 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
 else if (n == 0)
 {
-printf("Last digit of %d is %d is 0 \n", n, lastdigit);
+printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
 else
 {
