79d50292655201d2aca2ac025a2207a36b429775
Mickykore <mikiyasayele6@gmail.com>
2022-09-09 22:51:26 +0000

    add file vim 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; ++n)
+{
+putchar (n);
+putchar (',');
+putchar (' ');
+}
+putchar('\n');
+return (0);
+}
