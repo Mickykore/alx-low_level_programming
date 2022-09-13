1f60895ddf28bc64344503519971f89d3b86bfb7
Mickykore <mikiyasayele6@gmail.com>
2022-09-13 22:18:53 +0000

    edit file 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,16 @@
 int print_last_digit(int n)
 {
 int lastnumber;
+if (n < 0)
+{
+lastnumber = -1 * n % 10;
+_putchar(lastnumber + '0');
+return (lastnumber);
+}
+else
+{
 lastnumber = n % 10;
 _putchar(lastnumber + '0');
 return (lastnumber);
+}
 }
