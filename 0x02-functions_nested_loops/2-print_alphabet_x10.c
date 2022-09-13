85b8069a1a23edde27bf48d541599eec7e338b2b
Mickykore <mikiyasayele6@gmail.com>
2022-09-13 18:46:47 +0000

    create file 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print a-z 10x
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+char j = 'a';
+int i = 1;
+while (i <= 10)
+{
+while (j <= 'z')
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
