a385048db12b43752ec0f6116a9e2eb68280f67b
Mickykore <mikiyasayele6@gmail.com>
2022-09-13 22:09:50 +0000

    create file 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * @n: the number
+ *
+ * Return: n % 10
+ */
+int print_last_digit(int n)
+{
+int lastnumber;
+lastnumber = n % 10;
+_putchar(lastnumber);
+return (lastnumber);
+}
