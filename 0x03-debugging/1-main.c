b5ed704c38c18dc7ff6db5c95d1e214ab58c7261
Mickykore <mikiyasayele6@gmail.com>
2022-09-14 17:45:36 +0000

    edit file 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,11 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
-while (i < 10)
-{
-putchar(i);
-/* don't delete it! */
+/**
+ * while (i < 10)
+ * {
+ * putchar(i);
+ */
 }
 printf("Infinite loop avoided! \\o/\n");
 
