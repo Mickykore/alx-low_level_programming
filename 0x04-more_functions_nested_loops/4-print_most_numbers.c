245bc812a4a6c07cf9f489a01121f155dc70f5f2
Mickykore <mikiyasayele6@gmail.com>
2022-09-15 10:36:51 +0000

    add file 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - function to print most numbers
+ *
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+int i;
+	for (i = 48; i <= 57; i++)
+{
+	if (i != 50 || i != 52)
+	_putchar(i);
+}
+_putchar('\n');
+}
