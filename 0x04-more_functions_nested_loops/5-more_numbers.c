df7e6e18072eaaaaeef70b4190d1fd645f34b7ed
Mickykore <mikiyasayele6@gmail.com>
2022-09-15 18:25:01 +0000

    edit file 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ for (i = 0; i < 10; i++)
 {
 	for (j = 0; j <= 14; j++)
 {
-		if (i > 9)
+		if (j > 9)
 {
-		_putchar(i / 10 + '0');
+		_putchar(j / 10 + '0');
 }
-	_putchar(i % 10 + '0');
+	_putchar(j % 10 + '0');
 }
 _putchar('\n');
 }
