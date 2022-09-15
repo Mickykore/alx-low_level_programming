aac2d810b12f3d7becc4da9a70265d8cd898d924
Mickykore <mikiyasayele6@gmail.com>
2022-09-15 18:43:53 +0000

    add file 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - function to print a line
+ * @n: number of line to be printed
+ *
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+		while (n > 0)
+{
+		_putchar('\');
+		n--;
+}
+_putchar('\n');
+}
