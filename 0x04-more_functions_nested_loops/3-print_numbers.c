8332d882b28db5365f4b0a677aa87f6ed4274208
Mickykore <mikiyasayele6@gmail.com>
2022-09-15 10:07:31 +0000

    add file 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function to print no.
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int i;
+	for (i = 0; i <= 9; i++)
+{
+		_putchar(i);
+}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,6 @@
 int _isupper(int );
 int _isdigit(int );
 int mul(int, int);
+void print_numbers(void);
+int _putchar(char);
 #endif
