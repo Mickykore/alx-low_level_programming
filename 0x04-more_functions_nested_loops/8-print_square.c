3e060c8ea3ea78950e0e2bc9bb8c1270c9f2c407
Mickykore <mikiyasayele6@gmail.com>
2022-09-16 01:22:50 +0000

    add file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - function to print dquare
+ * @size: size of the square
+ *
+ * Return: nothing
+ */
+void print_square(int size)
+{
+int i, j;
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
