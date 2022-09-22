1767853047cd67c4651f4ccccfabceab747bef20
Mickykore <mikiyasayele6@gmail.com>
2022-09-22 19:57:59 +0000

    add file vim 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* string_toupper - Changes all lowercase letters to uppercase.
+* @str: The string to be changed.
+*
+* Return: A pointer to the changed string.
+ */
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+
+return (str);
+}
