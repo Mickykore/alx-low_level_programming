70f4f20aeb8c96db7c0a3f6e011aa04c7b32aeac
Mickykore <mikiyasayele6@gmail.com>
2022-09-28 19:40:47 +0000

    add file 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to evaluate string length
+ * @s: string to be evaluated
+ * Return: length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+int slen;
+if (*s == '\0')
+return (0);
+else
+{
+slen = 1 + _strlen_recursion(s + 1);
+}
+}
