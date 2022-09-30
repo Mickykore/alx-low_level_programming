454dd2cc27b7ac1ec199c543b89e9ecf7c6780ad
Mickykore <mikiyasayele6@gmail.com>
2022-09-30 18:03:32 +0000

    add file 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function that prints its name
+ * @argc: number of command line arguments
+ * @argv: the program command line arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+printf("program's name is: %s\n", argv[0]);
+return (0);
+}
