fb0291a7e04990bc244dcd6988de7f56322df123
Mickykore <mikiyasayele6@gmail.com>
2022-10-06 22:02:45 +0000

    edit main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,12 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
-void *malloc_checked(unsigned int b);
 int _putchar(char c);
-int len(char *str);:
+void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int len(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
