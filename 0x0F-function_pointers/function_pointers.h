51138a275fa7987c5d96e0d3cdd0838cfb7a5d37
Mickykore <mikiyasayele6@gmail.com>
2022-10-13 01:26:22 +0000

    edit file function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,7 +2,7 @@
 #define FUNCTION_POINTERS_H
 
 #include <stdio.h>
-
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iteration(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
