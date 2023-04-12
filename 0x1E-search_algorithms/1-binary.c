3ba54667c48acb7092f530e782e97744b293a1d3
mickykore <mikiyasayele6@gmail.com>
2023-04-12 10:52:05 +0300

    add file 1-binary.c to 0x1E-search_algorithms project

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+	int right = size - 1;
+	int mid, i;
+
+	while (left <= right)
+	{
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+				printf(", ");
+		}
+		printf("\n");
+		mid = (left + right) / 2;
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	return (-1);
+}
